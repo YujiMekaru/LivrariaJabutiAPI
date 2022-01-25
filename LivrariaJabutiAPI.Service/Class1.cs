edeabdbcddd4cecbd162db80bed0ffaa19240fae
Gustavo Yuji Mekaru <gustavoyujim@hotmail.com.br>
2022-01-25 14:55:15 -0300

    Add project files.

---
+++ LivrariaJabutiAPI.Service/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace LivrariaJabutiAPI.Service
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ LivrariaJabutiAPI/Program.cs
@@ -0,0 +1,42 @@
+using LivrariaJabutiAPI.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+#region DbConnection
+string connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
+options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+#endregion
+
+#region Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+#endregion
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
