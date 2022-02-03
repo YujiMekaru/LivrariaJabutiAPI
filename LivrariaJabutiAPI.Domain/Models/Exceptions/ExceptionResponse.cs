9f675ed6fec7dda8a16933a79d19a57ef2526089
Gustavo Yuji Mekaru <gustavoyujim@hotmail.com.br>
2022-02-03 03:15:48 -0300

    Removed source from ExceptionResponse

--- LivrariaJabutiAPI.Domain/Models/Exceptions/ExceptionResponse.cs
+++ LivrariaJabutiAPI.Domain/Models/Exceptions/ExceptionResponse.cs
@@ -9,12 +9,10 @@ namespace LivrariaJabutiAPI.Domain.Models.Exceptions
     public class ExceptionResponse 
     {
         public int ErrorCode { get; private set; }
-        public string? Source { get; private set; } = String.Empty;
         public string? Message { get; private set; } = String.Empty;
 
         public ExceptionResponse(Exception exception)
         {
-            Source = exception.Source;
             Message = exception.Message;
         }
     }
